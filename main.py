a91745fa46eb4a8f516efd0ab59941db7ca21ed1
Nischal Keshav <96449844+NischalKeshav@users.noreply.github.com>
2022-03-31 17:39:35 +0000

    commit

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-import pygame
+import pygame,sys
 pygame.init()
+
+
+Screen = pygame.display.set_mode((700,700))
+BG = (80,100,200)
+
+
 
 
 ###################Images#######################
 Europe = pygame.image.load("Europe.png")
+
+
+
+
+
+while 1:
+  
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+  
+  Screen.display(Europe,(0,0))
+  pygame.display.update()
+  
+  
